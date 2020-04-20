b628107a3b626e8b35ed7dfe540c94bd7d1f13e4
mudassirniyazi <mudassirniyazi52@gmail.com>
2020-04-20 17:48:34 +0530

    Initial commit for Configuration Properties.

---
+++ online-book-store/src/main/java/in/bushansirgur/online/book/store/OnlineBookStoreApplication.java
@@ -0,0 +1,14 @@
+package in.bushansirgur.online.book.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBookStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBookStoreApplication.class, args);
+		System.out.println("Online Book Store ...");
+	}
+
+}
